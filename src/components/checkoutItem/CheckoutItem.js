9e383d4e5b497c73af77d6b56ea84191475a45ff
Tunde Osborne <tunde.osborne@Mac-users-MacBook-Pro.local>
2020-05-02 21:36:46 +0100

    add remove item and add item to checkout component

--- src/components/checkoutItem/CheckoutItem.js
+++ src/components/checkoutItem/CheckoutItem.js
@@ -1,18 +1,47 @@
 import React from "react";
+import { connect } from "react-redux";
+import {
+  clearItemFromCart,
+  AddItemToCart,
+  removeItemFromCart,
+} from "../../redux/cart/cartActions";
 import "./checkoutItem.scss";
 
-const CheckoutItem = ({ cartItem: { name, price, quantity, imageUrl } }) => {
+const CheckoutItem = ({
+  cartItem,
+  clearItemFromCart,
+  AddItemToCart,
+  removeItemFromCart,
+}) => {
+  const { name, price, quantity, imageUrl } = cartItem;
   return (
     <div className="checkout-item">
       <div className="image-container">
         <img src={imageUrl} alt="item" />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItemFromCart(cartItem)}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => AddItemToCart(cartItem)}>
+          &#10095;
+        </div>
+      </span>
       <span className="price">{price}</span>
-      <div className="remove-button">&#10005;</div>
+      <div
+        className="remove-button"
+        onClick={() => clearItemFromCart(cartItem)}
+      >
+        &#10005;
+      </div>
     </div>
   );
 };
 
-export default CheckoutItem;
+export default connect(null, {
+  clearItemFromCart,
+  AddItemToCart,
+  removeItemFromCart,
+})(CheckoutItem);
--- src/redux/cart/cartActions.js
+++ src/redux/cart/cartActions.js
@@ -8,3 +8,13 @@ export const AddItemToCart = (item) => ({
   type: cartActionTypes.ADD_ITEMS,
   payload: item,
 });
+
+export const clearItemFromCart = (item) => ({
+  type: cartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item,
+});
+
+export const removeItemFromCart = (item) => ({
+  type: cartActionTypes.REMOVE_ITEM,
+  payload: item,
+});
